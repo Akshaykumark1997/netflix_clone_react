c12f6e697da9e639e724c354d65ef955a9e8b59c
Akshay kumar k <akshayajith40@gmail.com>
2023-01-03 13:20:24 +0530

    navbar,banner and cards added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import React from 'react';
 import './App.css';
+import Banner from './Components/Banner/Banner';
+import NavBar from './Components/NavBar/NavBar';
+import RowPost from './Components/RowPost/RowPost';
+
 
 function App() {
   return (
-    <h1>hiii</h1>
+    <div>
+      <NavBar/>
+      <Banner/>
+      <RowPost/>
+    </div>
   );
 }
 
